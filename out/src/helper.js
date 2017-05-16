90abcaca3a5cdf4195c02aad966be9c201d793b5
Maher Saif <clever.netman@gmail.com>
2017-05-16 03:30:00 +0300

    Show only migration name in QuickPick list
    Signed-off-by: Maher Saif <clever.netman@gmail.com>

--- out/src/helper.js
+++ out/src/helper.js
@@ -3,8 +3,9 @@ var vscode = require('vscode');
 var path = require('path');
 
 var RailsHelper = (function () {
-    function RailsHelper(migrations_path) {
+    function RailsHelper() {
         this.items = [];
+        this.migrations_path = path.join(vscode.workspace.rootPath, 'db/migrate')
         this.patterns = ['db/migrate/*'];
     }
 
@@ -14,18 +15,22 @@ var RailsHelper = (function () {
 
         vscode.workspace.findFiles(cur.toString(), null).then(function (res) {
             res.forEach(function (i) {
-                var fn = vscode.workspace.asRelativePath(i);
-                _self.items.push(fn);
+                var file_path = vscode.workspace.asRelativePath(i);
+                var file_name = file_path.replace(/db\/migrate\/[0-9]{14}_(.*)\.rb/, '$1');
+
+                _self.items.push(file_name);
             });
             if (arr.length > 0) {
                 _self.generateList(arr);
             }
             else {
-                _self.showQuickPick(_self.items);
+                _self.showQuickPick(_self.items.reverse());
             }
         });
     };
     RailsHelper.prototype.showQuickPick = function (items) {
+        var _self = this;
+
         var p = vscode.window.showQuickPick(items, {
             placeHolder: "Select File",
             matchOnDetail: true
@@ -33,7 +38,7 @@ var RailsHelper = (function () {
         p.then(function (value) {
             if (!value)
                 return;
-            var fn = vscode.Uri.parse('file://' + path.join(vscode.workspace.rootPath, value));
+            var fn = vscode.Uri.parse('file://' + path.join(_self.migrations_path, value));
             vscode.workspace.openTextDocument(fn).then(function (doc) {
                 return vscode.window.showTextDocument(doc);
             });
